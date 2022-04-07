f2806d3ca33caf1ecc2f641076842e97ea0da915
Roniel Roni <ronielvb@hotmail.com>
2022-04-06 22:07:41 -0300

    Login

--- src/components/BarraNavegacaoQuandoDeslogado.vue
+++ src/components/BarraNavegacaoQuandoDeslogado.vue
@@ -2,9 +2,6 @@
   <ul class="navbar-nav mr-auto">
     <li class="nav-item">
       <router-link class="nav-link" to="/login">Login</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link to="/cadastre-se" class="nav-link">Registre-se</router-link>
-    </li>
+    </li>    
   </ul>
 </template>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="container">
     <h1>Login</h1>
-    <form @submit.prevent="efetuarLogin">
-      <div class="form-group">
-        <label for="email">E-mail</label>
-        <input type="email" class="form-control" v-model="usuario.email" />
-      </div>
-      <div class="form-group">
-        <label for="senha">Senha</label>
-        <input type="password" class="form-control" v-model="usuario.password" />
-      </div>
-      <p class="alert alert-danger" v-if="mensagemErro"> {{ mensagemErro }}</p>
-      <button type="submit" class="btn btn-primary brn-block">Logar</button>
-     
-    </form>
+    <div class="col-sm-12">
+      <form @submit.prevent="efetuarLogin">
+        <div class="form-group">
+          <label for="email">E-mail</label>
+          <input type="email" class="form-control" v-model="usuario.email" />
+        </div>
+        <div class="form-group">
+          <label for="senha">Senha</label>
+          <input type="password" class="form-control" v-model="usuario.password" />
+        </div>
+        <p class="alert alert-danger" v-if="mensagemErro"> {{ mensagemErro }}</p>
+        <button type="submit" class="btn btn-primary brn-block">Logar</button>
+      
+      </form>
+    </div>
   </div>
 </template>
 
